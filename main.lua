3a51dec6949264685eea7bc756945641ed2cf23f
Jon Kenyon <jrkenyon2@yahoo.com>
2022-05-25 13:02:44 -0500

    Added slash commands for chat to call addon

---
+++ main.lua
@@ -0,0 +1,10 @@
+    SLASH_MUSKETEER1 = "/musketeer"
+    SLASH_MUSKETEER2 = "/Musketeer"
+    SLASH_MUSKETEER3 = "/MUSKETEER"
+    SLASH_MUSKETEER4 = "/musk"
+    SLASH_MUSKETEER5 = "/Musk"
+    SLASH_MUSKETEER6 = "/MUSK"
+
+
+
+    SlashCmdList["MUSKETEER"] = ;
